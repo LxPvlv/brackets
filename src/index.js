97e43a102068d9ead74e9a955b684841715b76c7
lxpvlv <w.lxpvlv@gmail.com>
2020-03-03 17:40:26 +0500

    feat: add solution for brakets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const close = [];
+    return str
+        .split("")
+        .every(bracket =>
+            bracketsConfig.some(bc =>
+                bracket !== bc[0]
+                    ? false
+                    : bc[0] === close[close.length - 1]
+                    ? (close.pop(), true)
+                    : (close.push(bc[1]), true)
+            )
+                ? true
+                : bracket === close.pop()
+        )
+        ? !close.length
+        : false;
+};
